bd0503719f97bfc993b08ddd598745eac63d2921
RuanMSantos <ruancauancauan@gmail.com>
2024-09-06 17:30:09 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int limiteContagem = 10;
+int contador = 0;
+
+Contador();
+
+void Contador()
+{
+    contador++;
+    Thread.Sleep(1000);
+    Console.Write($"{contador} ");
+    if (contador < limiteContagem)
+    {
+        Contador();
+    }
+
+    else 
+    {
+        return;
+    }
+}
